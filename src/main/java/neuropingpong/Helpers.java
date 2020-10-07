9f2d33cd96135756ebac0bf81af8c708b3d89e7e
Roman Didukh <roma.didukh@gmail.com>
2020-10-07 22:55:06 +0100

    Allow mutations of neurons.

--- src/main/java/neuropingpong/Helpers.java
+++ src/main/java/neuropingpong/Helpers.java
@@ -1,9 +1,15 @@
 package neuropingpong;
 
+import java.util.Random;
+
 class Helpers {
   static void checkArgument(boolean condition) {
     if (!condition) {
       throw new IllegalArgumentException();
     }
+  }
+
+  static double randomDouble(Random random, double minValue, double maxValue) {
+    return minValue + (maxValue - minValue) * random.nextDouble();
   }
 }
