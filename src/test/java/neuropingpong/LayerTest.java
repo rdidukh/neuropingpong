659a0677067b398148221e619b4778b2cdf3a92c
Roman Didukh <roma.didukh@gmail.com>
2020-10-12 22:48:15 +0100

    Implement NeuralNetwork. (#5)

--- src/test/java/neuropingpong/LayerTest.java
+++ src/test/java/neuropingpong/LayerTest.java
@@ -8,8 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class LayerTest {
@@ -40,16 +42,37 @@ class LayerTest {
     var layer = new Layer(NEURONS);
     Random mockRandom = spy(Random.class);
     when(mockRandom.nextInt(3)).thenReturn(1);
-    when(mockRandom.nextDouble()).thenReturn(1.0);
     layer.mutate(mockRandom, 0.5);
     var neurons = layer.getNeurons();
     assertEquals(NEURONS.get(0), neurons.get(0));
     assertNotEquals(NEURONS.get(1), neurons.get(1));
   }
 
+  @Test
+  void layerEquals() {
+    Layer layer =
+        newLayer(neuron(3.2, 1.1, -2.2, 3.3), neuron(0.4, -4.4, 5.5, 6.6));
+    assertEquals(layer,
+        newLayer(neuron(3.2, 1.1, -2.2, 3.3), neuron(0.4, -4.4, 5.5, 6.6)));
+    assertNotEquals(layer,
+        newLayer(neuron(3.2, 1.1, -2.2, 3.3), neuron(0.5, -4.4, 5.5, 6.6)));
+    assertNotEquals(layer,
+        newLayer(neuron(3.2, 1.1, -2.1, 3.3), neuron(0.4, -4.4, 5.5, 6.6)));
+    assertNotEquals(layer, newLayer(neuron(3.2, 1.1, -2.2, 3.3)));
+  }
+
   private void assertOutputsEqual(Layer layer, double... expectedOutputs) {
     assertArrayEquals(expectedOutputs,
         layer.getOutputs().stream().mapToDouble(Double::doubleValue).toArray(),
         0.0001);
+  }
+
+  static Neuron neuron(double bias, double... weights) {
+    return new Neuron(bias,
+        Arrays.stream(weights).boxed().collect(Collectors.toList()));
+  }
+
+  private static Layer newLayer(Neuron... neurons) {
+    return new Layer(Arrays.asList(neurons));
   }
 }
